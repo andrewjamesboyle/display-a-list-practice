86d2c65b26d7d28b35e008bc306000c6a121bc45
ANDREW JAMES BOYLE <ANDREWBOYLEMUSIC@GMAIL.COM>
2022-07-30 12:23:54 -0700

    basic basic basic css

--- renderCandy.js
+++ renderCandy.js
@@ -1,5 +1,6 @@
 export function renderCandy(candy) {
     const candiesEl = document.createElement('div');
     candiesEl.textContent = `Brand: ${candy}`;
+    candiesEl.classList.add('candy');
     return candiesEl;   
 }
